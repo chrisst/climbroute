a4192c9665d5a8055af1c13a48d64df8191e075e
Miles Dickson <mdickson@backcountry.com>
2014-03-12 10:00:58 -0600

    add/remove routes

--- climbroute.js
+++ climbroute.js
@@ -4,17 +4,19 @@ Routes = new Meteor.Collection('routes');
 if (Meteor.isServer) {
   Meteor.startup(function () {
     
-    // clear Routes collection
-    Routes.remove({});
-    
-    // import route data
-    var routes = {};
-    routes = JSON.parse(Assets.getText('routes.json'));
-    
-    // insert routes in collection
-    routes.forEach(function(route) {
-      Routes.insert(route);
-    });
+    if (Routes.find().fetch().length == 0) {
+      // clear Routes collection
+      Routes.remove({});
+      
+      // import route data
+      var routes = {};
+      routes = JSON.parse(Assets.getText('routes.json'));
+      
+      // insert routes in collection
+      routes.forEach(function(route) {
+        Routes.insert(route);
+      });
+    }
 	
   });
 }
@@ -34,6 +36,26 @@ if (Meteor.isClient) {
   
   Template.Routes.Route = function() {
     return Routes.find({});
+  }
+  
+  Template.Routes.events = {
+    'click .add': function(e) {
+      e.preventDefault();
+
+      var name   = $('.name').val();
+      var rating = $('.rating').val();
+      var setter = $('.setter').val();
+      
+      if (name.length > 0 && rating.length > 0 && setter.length > 0 ) {
+        Routes.insert({"name":name,"rating":rating,"setter":setter});
+        $('input').val('');
+      }
+    },
+    'click .remove': function(e) {
+      e.preventDefault();
+      var route_id = this._id;
+      Routes.remove(route_id);
+    }
   }
 
   Accounts.ui.config({
